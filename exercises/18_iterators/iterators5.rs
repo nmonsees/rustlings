516a5e9b272503f07bf17ae6051839d39923cd2a
Nick Monsees <nickmonsees@protonmail.com>
2024-12-30 09:49:33 -0800

    18_iterators

--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -28,6 +28,8 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // `map` is a hash map with `String` keys and `Progress` values.
     // map = { "variables1": Complete, "from_str": None, … }
+    // map.values().fold(0, |acc, val| if *val == value { acc + 1 } else { acc })
+    map.values().filter(|val| **val == value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -46,8 +48,9 @@ fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progres
 // iterator instead of a `for` loop.
 fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
     // `collection` is a slice of hash maps.
-    // collection = [{ "variables1": Complete, "from_str": None, … },
-    //               { "variables2": Complete, … }, … ]
+    // collection = [{ "variables1": Complete, "from_str": None, … }, //               { "variables2": Complete, … }, … ]
+    // collection.iter().fold(0, |acc, coll| acc + count_iterator(coll, value)) }
+    collection.iter().map(|map| count_iterator(map, value)).sum()
 }
 
 fn main() {
